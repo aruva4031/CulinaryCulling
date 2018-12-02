892a820aa71807607595cde95027b7af420e94af
aruva4031 <aruvalcaba@csumb.edu>
2018-12-02 13:56:22 -0800

    added the level

---
+++ CulinaryCulling/Assets/Scripts/ActivatableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatableObject : MonoBehaviour {
+	private bool circuit_on = false;
+	public bool fan;
+	public bool platform;
+	public float speed;
+	public GameObject platform_goal;
+	private bool goalReached = false;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (circuit_on) {
+			if (fan) {
+				transform.Rotate (0,0,1.5f * speed);
+			} 
+			else if (platform) {
+				
+				if (!goalReached) {
+					rb.AddForce (0, speed, 0);
+				}
+				if (transform.position.y >= platform_goal.transform.position.y) {
+					goalReached = true;
+					rb.AddForce (0, 0, 0);
+				}
+				if (transform.position.y <= platform_goal.transform.position.y) {
+					goalReached = false;
+				}
+			}
+			else {
+				Debug.Log ("Need to select type of object");
+			}
+
+		}
+	}
+	public void Switch(bool power) {
+		circuit_on = power;
+	}
+}
---
+++ CulinaryCulling/Assets/Scripts/Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+	public GameObject[] ActivatedObjects;
+	private bool circuit_on = false;
+	public Material isOn;
+	public Material isOff;
+	private void Activate (bool power){
+		foreach (GameObject platform in ActivatedObjects) {
+			platform.GetComponent<ActivatableObject> ().Switch (power);
+			//pass in power for objects to activate	
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.transform.tag == "spatuala" || other.transform.tag == "fryingPan"){
+			circuit_on = !circuit_on;
+			if (circuit_on) {
+				this.GetComponent<MeshRenderer> ().material = isOn;
+			} 
+			else {
+				this.GetComponent<MeshRenderer> ().material = isOff;
+			}
+			Activate (circuit_on);
+		}
+	}
+}
