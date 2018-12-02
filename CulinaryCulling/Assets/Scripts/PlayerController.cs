443e64b561162592fe471c36e6006a664d7d27d7
connmatt <matt.connolly1225@gmail.com>
2018-12-02 11:19:03 -0800

    Added the enemies: tomato, lettuce with the scripts for enemies

--- CulinaryCulling/Assets/Scripts/PlayerController.cs
+++ CulinaryCulling/Assets/Scripts/PlayerController.cs
@@ -9,203 +9,61 @@ public class PlayerController : MonoBehaviour {
     public Vector3 upperBorder;
     public Vector3 lowerBorder;
 
-    public GameObject spatulaLeft;
-    public GameObject spatulaRight;
-    public GameObject fryingPanLeft;
-    public GameObject fryingPanRight;
+    public GameObject spatula;
+    public GameObject fryingPan;
     public Camera cam;
-
     bool panWithPlayer;
-    //Transform panPos;
-
     bool playerJumps;
-
-    public bool rotate;
-    public float rotateAmount;
-    public float rotateAngle;
-    public float duration;
-
-    public bool rotateBack;
 
     Vector3 direction;
 
-    bool clicked;
-    bool doubleclicked;
-    float tempTime;
-    public float speed;
-
-    public GameObject mousePoint;
-
-    public Vector3 spatulaPos1;
-    public Vector3 spatulaPos2;
-
-    //private enum playerState { IDLE,WALKING, JUMPING, SPATULA_ATTACK, FRYING_PAN_ATTACK };
-    //private playerState current_state = playerState.IDLE;
+    private enum playerState { IDLE,WALKING, JUMPING, SPATULA_ATTACK, FRYING_PAN_ATTACK };
+    private playerState current_state = playerState.IDLE;
     private Rigidbody rb;
-
-    public bool playerFreeze;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-        spatulaRight= transform.GetChild(0).gameObject;
-        spatulaLeft = transform.GetChild(1).gameObject;
-        //fryingPan = transform.GetChild(1).transform.GetChild(0).gameObject;
-        //panPos = fryingPan.transform;
+        spatula = transform.GetChild(0).gameObject;
+        fryingPan = transform.GetChild(1).gameObject;
         panWithPlayer = true;
         playerJumps = false;
-        rotate = false;
-        clicked = false;
-        doubleclicked = false;
-        speed = 0.1f;
-        playerFreeze = false;
-        //spatulaPos1= new Vector3(spatula.transform.position.x - 21.4f, spatula.transform.position.y + 2.5f, spatula.transform.position.z);
-        //spatulaPos2 = spatula.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (playerFreeze == false)
-        {
-            if ((transform.position.x >= (leftBorder.x + 1f)))
+        Debug.Log(rb.velocity);
+        if ((transform.position.x>=(leftBorder.x+1f))&& (transform.position.x <= (rightBorder.x - 1f))) {
+            if (Input.GetKey(KeyCode.A))
             {
-                //movement to left
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    spatulaLeft.gameObject.SetActive(true);
-                    spatulaRight.gameObject.SetActive(false);
-                    GetComponent<BefriendEnemy>().isLeft = true;
-                    GetComponent<BefriendEnemy>().directionAmount=-2;
-                    //spatula.transform.rotation = Quaternion.Euler(spatula.transform.rotation.x, 180, spatula.transform.rotation.z);
-                    //spatula.transform.position = spatulaPos1;
-                }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
-                }
+                transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
             }
-            //if you are within the border constraint of left
-            if (transform.position.x <= (rightBorder.x - 1f))
+            if (Input.GetKey(KeyCode.D))
             {
-                ////if you didn't click before, start a timer
-                //if (Input.GetKeyDown(KeyCode.D) && !clicked)
-                //{
-                //    tempTime = Time.time;
-                //    clicked = true;
-                //}
-                ////if the distance between the two clicks is close enough, do the double click
-                //else if (Input.GetKey(KeyCode.D) && ((Time.time - tempTime) <= 1f) && clicked)
-                //{
-                //    speed = 0.3f;
-                //    doubleclicked = true;
-                //    clicked = false;
-                //}
-                //else if (Input.GetKey(KeyCode.D) && ((Time.time - tempTime) > 1f) && clicked)
-                //{
-                //    clicked = false;
-                //}
-
-                //movement to right
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    GetComponent<BefriendEnemy>().isLeft = false;
-                    GetComponent<BefriendEnemy>().directionAmount = 2;
-                    spatulaRight.gameObject.SetActive(true);
-                    spatulaLeft.gameObject.SetActive(false);
-                    //spatula.transform.rotation = Quaternion.Euler(spatula.transform.rotation.x, 180, spatula.transform.position.z);
-                    //spatula.transform.position = spatulaPos2;
-                }
-                if (Input.GetKey(KeyCode.D))
-                {
-                    transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
-                }
-                //else if (Input.GetKeyUp(KeyCode.D) && doubleclicked)
-                //{
-                //    speed = 0.1f;
-                //    doubleclicked = false;
-                //    clicked = false;
-                //}
-            }
-            //else if (Input.GetKeyUp(KeyCode.D) && doubleclicked)
-            //{
-            //    speed = 0.1f;
-            //    clicked = false;
-            //}
-            //else if (transform.transform.position.x > (leftBorder.x + 1f))
-            //{
-
-            //}
-            if (!playerJumps)
-            {
-                if (Input.GetKeyDown(KeyCode.Space) && !playerJumps)
-                {
-                    rb.velocity = transform.up * 6f;
-                    playerJumps = true;
-                }
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("In");
-                //spatulaLeft.SetActive(false);
-                //spatulaRight.SetActive(false);
-                if (spatulaLeft.activeSelf)
-                {
-                    spatulaLeft.SetActive(false);
-                    fryingPanLeft.SetActive(true);
-                    fryingPanLeft.GetComponent<Rigidbody>().useGravity = false;
-                    fryingPanLeft.GetComponent<FryingPanScript>().tempTime = Time.time;
-                }
-                else if (spatulaRight.activeSelf)
-                {
-                    fryingPanRight.SetActive(true);
-                    spatulaRight.SetActive(false);
-                    fryingPanRight.GetComponent<Rigidbody>().useGravity = false;
-                    fryingPanRight.GetComponent<FryingPanScript>().tempTime = Time.time;
-                }
-            }
-
-            //right mouse: attack with spatula
-            if (Input.GetMouseButtonDown(1))
-            {
-                //spatula.SetActive(true);
-                rotate = true;
-            }
-
-            //spatula rotation
-            if (spatulaLeft.activeSelf == true)
-            {
-                spatulaRotation(spatulaLeft);
-            }
-            else if(spatulaRight.activeSelf == true)
-            {
-                spatulaRotation(spatulaRight);
+                transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
             }
         }
-    }
-
-    void spatulaRotation(GameObject spatula)
-    {
-        Vector3 eulerRotation = spatula.transform.rotation.eulerAngles;
-
-        if (rotate && eulerRotation.z > 220f)
+        if (!playerJumps)
         {
-            spatula.transform.Rotate(0, 0, -rotateAmount);
+            if (Input.GetKeyDown(KeyCode.Space) && !playerJumps)
+            {
+                rb.velocity = transform.up * 6f;
+                //transform.position = new Vector3(transform.position.x, transform.position.y+0.2f, transform.position.z);
+                playerJumps = true;
+            }
         }
-        if (rotate && eulerRotation.z <= 220f)
+        //left mouse: throw frying pan
+        if (Input.GetMouseButtonDown(0)&&panWithPlayer)
         {
-            rotate = false;
-            StartCoroutine(backToIdle(duration));
+            fryingPan.SetActive(true);
+            direction = new Vector3(fryingPan.transform.position.x + 2f, fryingPan.transform.position.y + 3f, fryingPan.transform.position.z);
+            fryingPan.GetComponent<Rigidbody>().AddForce(direction * direction.magnitude * 20);
+            panWithPlayer = false;
         }
-
-        if (rotateBack && eulerRotation.z < 330f)
+        //right mouse: attack with spatula
+        if (Input.GetMouseButtonDown(1))
         {
-            spatula.transform.Rotate(0, 0, rotateAmount);
-
-        }
-        if (rotateBack && eulerRotation.z >= 330f)
-        {
-            rotateBack = false;
-            //spatula.SetActive(false);
+            spatula.SetActive(true);
         }
     }
 
@@ -216,18 +74,5 @@ public class PlayerController : MonoBehaviour {
             playerJumps = false;
         }
     }
-
-    IEnumerator backToIdle(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        rotateBack = true;
-    }
-    //IEnumerator returnPan(float duration)
-    //{
-    //    yield return new WaitForSeconds(duration);
-    //    fryingPan.SetActive(false);
-    //    fryingPan.transform.position = panPos.position;
-    //    mousePoint.SetActive(false);
-    //}
 
 }
